8ad069ce0cc5a2b0531e60f52993e2f7306e9422
Edwards Moses <edwardsmoses3@gmail.com>
2021-11-16 11:04:52 +0100

    Completed Reading Image Data in Base64

--- public/main.js
+++ public/main.js
@@ -1,1 +1,16 @@
-console.log('test');
+const init = () => {
+  const input = document.getElementById("upload");
+
+  const fileReader = new FileReader();
+  fileReader.addEventListener("load", () => {
+    const data = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/, "");
+    console.log(data);
+  });
+
+  input.addEventListener("change", () => {
+    const file = input.files[0];
+    fileReader.readAsDataURL(file);
+  });
+};
+
+init();
