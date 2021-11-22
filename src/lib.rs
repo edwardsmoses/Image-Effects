b9c7a318aa66ffcf475ec28d7209f125d4500f15
Edwards Moses <edwardsmoses3@gmail.com>
2021-11-22 09:06:07 +0100

    Learnt about installing packages. and decoding base64 images

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,10 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as j_log;
+use base64::decode;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str){
-    // let mut file = File::open(encoded_file).unwrap();
-    // let mut contents = String::new();
-    // file.read_to_string(&mut contents).unwrap();
-    // let mut encoded_image = Image::new(contents);
-    // encoded_image.grayscale();
-    // encoded_image.save_to_file(encoded_file);
+    j_log(&"Grayscale called".into());
 
-    j_log(&encoded_file.into());
-
+    let base64_to_vector = decode(encoded_file).unwrap();
 }
