d82915940311f8a9e21a72afdcfdddb3584c1afc
Edwards Moses <edwardsmoses3@gmail.com>
2021-11-22 09:10:01 +0100

    Loaded the image into memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as j_log;
 use base64::decode;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str){
     j_log(&"Grayscale called".into());
 
     let base64_to_vector = decode(encoded_file).unwrap();
+
+    let img = load_from_memory(&base64_to_vector).unwrap();
 }
